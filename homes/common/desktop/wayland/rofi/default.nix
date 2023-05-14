2e7dde6956921736ad12ce985f01fb09eafd69e6
Luna Simons <luna@bddvlpr.com>
2023-05-14 17:11:47 +0000

    refactor: rofi styling

--- homes/common/desktop/wayland/rofi/default.nix
+++ homes/common/desktop/wayland/rofi/default.nix
@@ -3,5 +3,21 @@
 {
   programs.rofi = {
     enable = true;
+
+    theme = ./theme.rasi;
+    extraConfig = {
+      modi = "run,drun,window";
+      icon-theme = "Oranchelo";
+      show-icons = true;
+      terminal = "kitty";
+      drun-display-format = "{icon} {name}";
+      location = 0;
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-window = " 﩯  Window";
+      display-Network = " 󰤨  Network";
+      sidebar-mode = true;
+    };
   };
 }
