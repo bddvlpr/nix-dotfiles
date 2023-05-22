9fffb17c3769e22382e3f84caaf07c89ac1a3826
Luna Simons <luna@bddvlpr.com>
2023-05-22 14:42:20 +0000

    fix: themed mako

--- homes/common/desktop/wayland/mako/default.nix
+++ homes/common/desktop/wayland/mako/default.nix
@@ -3,6 +3,14 @@
 {
   services.mako = {
     enable = true;
-    #TODO: styling
+
+    backgroundColor = "#24273a";
+    textColor = "#cad3f5";
+    borderColor = "#8aadf4";
+    progressColor = "#363a4f";
+
+    extraConfig = ''
+     [urgency=high]
+     border-color=#f5a97f'';
   };
 }
