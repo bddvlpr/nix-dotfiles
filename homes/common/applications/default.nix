4f518b13f27413d590aa7d4b0b177b80406f697f
Luna Simons <luna@bddvlpr.com>
2023-05-30 08:50:05 +0000

    feat: added nvim

--- homes/common/applications/default.nix
+++ homes/common/applications/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./media/spotify.nix
     ./entertainment/steam.nix
+    ./editors/nvim.nix
     ./editors/vscode.nix
     ./blender.nix
     ./discord.nix
---
+++ homes/common/applications/editors/nvim.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    withPython3 = false;
+    withNodeJs = false;
+    withRuby = false;
+
+    plugins = with pkgs.vimPlugins; [
+      presence-nvim
+    ];
+  };
+
+  home.sessionVariables.EDITOR = "nvim";
+}
