6e8c8ee5c6a0e115b59b18d5deb6f762ea00e42a
Luna Simons <luna@bddvlpr.com>
2023-06-16 21:56:40 +0200

    feat: border animations

--- homes/common/desktop/hyprland/config.nix
+++ homes/common/desktop/hyprland/config.nix
@@ -60,6 +60,7 @@ in ''
     bezier = overshot, 0.05, 0.9, 0.1, 1.05
     bezier = smoothOut, 0.36, 0, 0.66, -0.56
     bezier = smoothIn, 0.25, 1, 0.5, 1
+    bezier = linear, 0, 0, 1.0, 1.0
 
     animation = windows, 1, 5, overshot, slide
     animation = windowsOut, 1, 4, smoothOut, slide
@@ -68,6 +69,7 @@ in ''
     animation = fade, 1, 10, smoothIn
     animation = fadeDim, 1, 10, smoothIn
     animation = workspaces, 1, 6, default
+    animation = borderangle, 1, 100, linear, loop
   }
 
   dwindle {
