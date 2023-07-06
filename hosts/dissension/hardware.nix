0e7a5d19a3d1307f353cceb0dda73691833efd4f
Luna Simons <luna@bddvlpr.com>
2023-07-06 22:51:56 +0200

    chore: use amd microcode

--- hosts/dissension/hardware.nix
+++ hosts/dissension/hardware.nix
@@ -57,5 +57,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
