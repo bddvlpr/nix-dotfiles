8fdf6bf3cc85de62460d1b514995e75f3fbd73f3
Luna Simons <luna@bddvlpr.com>
2023-07-07 16:18:46 +0200

    fix: re-enable keybinds

--- homes/common/cli/editors/neovim/config/general.lua
+++ homes/common/cli/editors/neovim/config/general.lua
@@ -8,15 +8,15 @@ vim.o.autoindent = "smartindent"
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ";"
 
---vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('v', '<C-s>', '<C-c>:w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-s>', '<C-c>:w<CR>', { noremap = true, silent = true })
 
---vim.api.nvim_set_keymap('n', '<C-c>', '"+y', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
 
---vim.api.nvim_set_keymap('i', '<C-v>', '<Esc>"+p', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('v', '<C-v>', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-v>', '<Esc>"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-v>', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-v>', '"+p', { noremap = true, silent = true })
 
---vim.api.nvim_set_keymap('n', '<C-x>', '"+x', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('v', '<C-x>', '"+x', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-x>', '"+x', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-x>', '"+x', { noremap = true, silent = true })
