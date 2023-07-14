67cf812ca5027bfbabb748190964e61c0214a99a
Luna Simons <luna@bddvlpr.com>
2023-07-14 16:46:19 +0200

    fix: monitor pos

--- hosts/dissension/home-manager.nix
+++ hosts/dissension/home-manager.nix
@@ -12,25 +12,25 @@
   monitors = [
     {
       # Left monitor
-      name = "HDMI-A-1";
-      width = 3440;
-      height = 1440;
+      name = "DP-6";
+      width = 1920;
+      height = 1080;
       x = 0;
-      refreshRate = 100;
+      refreshRate = 165;
       workspace = "1";
     }
     {
       # Center monitor
-      name = "DP-3";
-      width = 1920;
-      height = 1080;
-      x = 3440;
-      refreshRate = 165;
+      name = "HDMI-A-2";
+      width = 3440;
+      height = 1440;
+      x = 1920;
+      refreshRate = 100;
       workspace = "2";
     }
     {
       # Right monitor
-      name = "DP-1";
+      name = "DP-4";
       width = 1920;
       height = 1080;
       x = 5360;
