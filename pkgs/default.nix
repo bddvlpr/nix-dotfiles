2e127f7cacc6a20b69e8d53daa8e74afab9677ae
Luna Simons <luna@bddvlpr.com>
2023-07-16 19:43:38 +0200

    fix: use upstream vrc-get

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,3 @@
 {pkgs ? (import ../nixpkgs.nix) {}}: {
-  # TODO: While https://github.com/NixOS/nixpkgs/pull/234279 is in PR...
-  vrc-get = pkgs.callPackage ./vrc-get.nix {};
   proton-ge-custom = pkgs.callPackage ./proton-ge-custom.nix {};
 }
