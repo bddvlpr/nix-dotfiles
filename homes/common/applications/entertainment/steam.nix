7e516656594463f915b1977f6924b4a41a066826
Luna Simons <luna@bddvlpr.com>
2023-07-30 23:24:03 +0200

    feat: add steam-run and proton-ge to scope

--- homes/common/applications/entertainment/steam.nix
+++ homes/common/applications/entertainment/steam.nix
@@ -5,14 +5,17 @@
         {
           extraProfile = "export STEAM_EXTRA_COMPAT_TOOLS_PATHS='${pkgs.proton-ge-custom}'";
         })
+      steam-run
       gamescope
       protontricks
       mangohud
+      proton-ge-custom
     ];
 
     persistence."/nix/persist/home/bddvlpr" = {
       allowOther = true;
       directories = [
+        ".factorio"
         ".local/share/Steam"
       ];
     };
