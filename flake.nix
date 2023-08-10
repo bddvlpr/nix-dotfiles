baffde542a4e9bc9a707e258e0ded90aeccf5ee7
Luna Simons <luna@bddvlpr.com>
2023-08-10 16:18:42 +0200

    fix: remove darwin support

--- flake.nix
+++ flake.nix
@@ -38,10 +38,7 @@
     inherit (self) outputs;
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
     ];
 
     mkSystem = modules:
