fa1bf2f2d27209ee785e5d88284a53b144e9d5ac
Luna Simons <luna@bddvlpr.com>
2023-08-13 23:05:05 +0200

    feat: fix discocss with temp override

--- homes/common/applications/communication/discord.nix
+++ homes/common/applications/communication/discord.nix
@@ -1,7 +1,22 @@
 {pkgs, ...}: {
-  home = {
-    packages = [pkgs.discord];
+  programs.discocss = {
+    enable = true;
 
-    persistence."/nix/persist/home/bddvlpr".directories = [".config/discord"];
+    package = pkgs.discocss.overrideAttrs (old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "bddvlpr";
+        repo = "discocss";
+        rev = "610302e25a420b8cd6f08b8faff909685d2f79fd";
+        hash = "sha256-2K7SPTvORzgZ1ZiCtS5TOShuAnmtI5NYkdQPRXIBP/I=";
+      };
+    });
+    discordPackage = pkgs.discord.override {withVencord = true;};
+
+    css = builtins.readFile (builtins.fetchurl {
+      url = "http://web.archive.org/web/20230813135259id_/https://catppuccin.github.io/discord/dist/catppuccin-macchiato-lavender.theme.css";
+      sha256 = "0kys50cz7z1y32xx332l3xq3lp4ybfls6njslp3xqjdwnhzvg16v";
+    });
   };
+
+  home.persistence."/nix/persist/home/bddvlpr".directories = [".config/discord"];
 }
