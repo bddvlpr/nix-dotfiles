b92cc2527d0fb11f95bb836629fec1931a56031c
Luna Simons <luna@bddvlpr.com>
2023-08-17 15:21:06 +0200

    feat: added gamemode

--- hosts/clients/dissension/default.nix
+++ hosts/clients/dissension/default.nix
@@ -9,6 +9,7 @@
     ../../common/modules/audio.nix
     ../../common/modules/client.nix
     ../../common/modules/docker.nix
+    ../../common/modules/gamemode.nix
     ../../common/modules/libvirtd.nix
     ../../common/modules/nvidia.nix
     ../../common/modules/opentabletdriver.nix
