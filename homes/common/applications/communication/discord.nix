1458cd285232c00d0db12acfe05ce9888f11b5ad
Luna Simons <luna@bddvlpr.com>
2023-08-20 15:54:47 +0200

    fix: pin to release of discocss

--- homes/common/applications/communication/discord.nix
+++ homes/common/applications/communication/discord.nix
@@ -2,11 +2,12 @@
   programs.discocss = {
     enable = true;
 
-    package = pkgs.discocss.overrideAttrs (old: {
+    package = pkgs.discocss.overrideAttrs (old: rec {
+		  version = "0.3.0";
       src = pkgs.fetchFromGitHub {
         owner = "bddvlpr";
         repo = "discocss";
-        rev = "610302e25a420b8cd6f08b8faff909685d2f79fd";
+        rev = "v${version}";
         hash = "sha256-2K7SPTvORzgZ1ZiCtS5TOShuAnmtI5NYkdQPRXIBP/I=";
       };
     });
