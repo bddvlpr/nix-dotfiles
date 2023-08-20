8550041f9283da56f67bfeab5a8ff64b65210bc9
Luna Simons <luna@bddvlpr.com>
2023-08-20 12:54:52 +0200

    fix: real-ip-header for CF

--- hosts/servers/alpha/modules/nginx.nix
+++ hosts/servers/alpha/modules/nginx.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  lib,
+  ...
+}: {
   services.nginx = {
     enable = true;
 
@@ -6,6 +10,23 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+
+    commonHttpConfig = let
+      realIpsFromList = lib.strings.concatMapStringsSep "\n" (x: "set_real_ip_from  ${x};");
+      fileToList = x: lib.strings.splitString "\n" (builtins.readFile x);
+      cfipv4 = fileToList (pkgs.fetchurl {
+        url = "https://www.cloudflare.com/ips-v4";
+        sha256 = "0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
+      });
+      cfipv6 = fileToList (pkgs.fetchurl {
+        url = "https://www.cloudflare.com/ips-v6";
+        sha256 = "1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
+      });
+    in ''
+         ${realIpsFromList cfipv4}
+      ${realIpsFromList cfipv6}
+      real_ip_header CF-Connecting-IP;
+    '';
   };
 
   security.acme = {
