326edeaf2f316a5a32abce086bc11d53bb4aa581
Luna Simons <luna@bddvlpr.com>
2023-11-06 09:10:21 +0100

    chore: re-enabled mullvad

--- hosts/common/modules/mullvad.nix
+++ hosts/common/modules/mullvad.nix
@@ -1,6 +1,5 @@
 {
-  # TODO: awaiting PR https://github.com/NixOS/nixpkgs/issues/263445
-  #services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.enable = true;
 
   environment.persistence."/nix/persist".directories = ["/etc/mullvad-vpn"];
 }
