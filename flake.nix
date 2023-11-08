1f4499b7c39d1998408dc5fdfb9a383d31057e0c
Luna Simons <luna@bddvlpr.com>
2023-11-08 18:26:50 +0100

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     hardware.url = "github:nixos/nixos-hardware";
     agenix.url = "github:ryantm/agenix";
 
-    untis-ics-sync.url = "github:bddvlpr/untis-ics-sync/v0.5.4";
+    untis-ics-sync.url = "github:bddvlpr/untis-ics-sync/v0.5.6";
 
     nix-spicetify.url = "github:the-argus/spicetify-nix";
     nix-index-database.url = "github:mic92/nix-index-database";
