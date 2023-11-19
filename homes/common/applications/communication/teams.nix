4aaa67b65c1a8c6df616c61644acec4f5ed4c65a
Luna Simons <luna@bddvlpr.com>
2023-11-19 18:41:02 +0100

    feat: re-enable teams

--- homes/common/applications/communication/teams.nix
+++ homes/common/applications/communication/teams.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home = {
-    # TODO: awaiting PR https://github.com/NixOS/nixpkgs/pull/263531
-    #packages = with pkgs; [teams-for-linux];
+    packages = with pkgs; [teams-for-linux];
 
     persistence."/nix/persist/home/bddvlpr".directories = [
       ".config/teams-for-linux"
