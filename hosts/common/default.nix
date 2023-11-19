3d1694c4df31db9b1ac2cf82fde135feb9707e8a
Luna Simons <luna@bddvlpr.com>
2023-11-19 21:03:02 +0100

    feat: added garnix cache

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -25,6 +25,12 @@
       auto-optimise-store = true;
       warn-dirty = false;
       trusted-users = ["bddvlpr"];
+      substituters = [
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
   };
 
