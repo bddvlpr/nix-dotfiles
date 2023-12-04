5cf176817559f31226aa96c61a77de13159de0fc
Luna Simons <luna@bddvlpr.com>
2023-12-04 16:19:46 +0100

    feat: re-enable freecad

--- homes/common/applications/productivity/freecad.nix
+++ homes/common/applications/productivity/freecad.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home = {
-    # TODO: awaiting PR https://github.com/NixOS/nixpkgs/pull/263599
-    #packages = with pkgs; [freecad];
+    packages = with pkgs; [freecad];
 
     persistence."/nix/persist/home/bddvlpr".directories = [".local/share/FreeCAD" ".config/FreeCAD"];
   };
