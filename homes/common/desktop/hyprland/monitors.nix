5433773e1d28ced331b218da21eca31de96415e7
Luna Simons <luna@bddvlpr.com>
2023-12-12 14:12:13 +0100

    style: fix formatting issues

--- homes/common/desktop/hyprland/monitors.nix
+++ homes/common/desktop/hyprland/monitors.nix
@@ -9,6 +9,7 @@ in
   lib.concatStringsSep "\n" (lib.forEach enabledMonitors (m: ''
     monitor=${m.name},${toString m.width}x${toString m.height}@${toString m.refreshRate},${toString m.x}x${toString m.y},${toString m.scale}
     ${lib.optionalString (m.workspace != null) "workspace=${m.name},${m.workspace}"}
-  '')) + ''
-  monitor=,highres,auto,1
+  ''))
+  + ''
+    monitor=,highres,auto,1
   ''
