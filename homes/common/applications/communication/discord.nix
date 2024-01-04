acd3a053a2987fbc7397cd64020b4590ec5655d0
Luna Simons <luna@bddvlpr.com>
2024-01-04 13:17:27 +0100

    refactor: remove discocss :pray:

--- homes/common/applications/communication/discord.nix
+++ homes/common/applications/communication/discord.nix
@@ -1,24 +1,7 @@
 {pkgs, ...}: {
-  programs.discocss = {
-    enable = true;
+  home = {
+    packages = [(pkgs.discord.override {withVencord = true;})];
 
-    package = pkgs.discocss.overrideAttrs (old: rec {
-      version = "0.3.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "bddvlpr";
-        repo = "discocss";
-        rev = "v${version}";
-        hash = "sha256-BFTxgUy2H/T92XikCsUMQ4arPbxf/7a7JPRewGqvqZQ=";
-      };
-    });
-
-    discordPackage = pkgs.discord.override {withVencord = true;};
-
-    css = builtins.readFile (builtins.fetchurl {
-      url = "https://web.archive.org/web/20231102191925id_/https://catppuccin.github.io/discord/dist/catppuccin-macchiato-lavender.theme.css";
-      sha256 = "0z1pwfq5w2f2l2p5bry81sldrl8hpc5yn5q6xcyg3kq1fp9nbq3d";
-    });
+    persistence."/nix/persist/home/bddvlpr".directories = [".config/discord" ".config/Vencord"];
   };
-
-  home.persistence."/nix/persist/home/bddvlpr".directories = [".config/discord" ".config/Vencord"];
 }
