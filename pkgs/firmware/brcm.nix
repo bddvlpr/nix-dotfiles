8008d63f9a964fdd52fc2b26bedfdb831dce4f66
Luna Simons <luna@bddvlpr.com>
2024-02-02 19:32:45 +0100

    fix: oops

--- pkgs/firmware/brcm.nix
+++ pkgs/firmware/brcm.nix
@@ -1,16 +1,19 @@
-{fetchurl, stdenvNoCC, ...}:
-let
+{
+  fetchurl,
+  stdenvNoCC,
+  ...
+}: let
   firmware = fetchurl {
     url = "https://files.catbox.moe/y6pz3d.gz";
     hash = "sha256-z/lPdVfiWHWsQDTSE1mR6A5/hg7+JnbmblSXE/oyn10=";
   };
 in
-stdenvNoCC.mkDerivation {
-  name = "brcm";
+  stdenvNoCC.mkDerivation {
+    name = "brcm";
 
-  buildCommand = ''
-    target="$out/lib/firmware"
-    mkdir -p "$target"
-    tar -xf ${firmware} -C $target
-  '';
-}
+    buildCommand = ''
+      target="$out/lib/firmware"
+      mkdir -p "$target"
+      tar -xf ${firmware} -C $target
+    '';
+  }
