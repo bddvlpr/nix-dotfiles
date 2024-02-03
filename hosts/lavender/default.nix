d9b62bb9cb29e07cce056bc6b867985547523c6c
Luna Simons <luna@bddvlpr.com>
2024-02-03 01:49:34 +0100

    fix: re-enable expensive modules

--- hosts/lavender/default.nix
+++ hosts/lavender/default.nix
@@ -7,14 +7,12 @@
   imports = [
     ../common
     ../common/modules/amd.nix
-    #../common/modules/adb.nix
     ../common/modules/audio.nix
-    #../common/modules/bluetooth.nix
+    ../common/modules/bluetooth.nix
     ../common/modules/client.nix
-    #../common/modules/docker.nix
-    #../common/modules/libvirtd.nix
-    #../common/modules/mullvad.nix
-    #../common/modules/nvidia.nix
+    ../common/modules/docker.nix
+    ../common/modules/libvirtd.nix
+    ../common/modules/mullvad.nix
     ../common/modules/thunar.nix
     ../common/modules/xdg.nix
     ./hardware.nix
