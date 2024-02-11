e86614449aeddc1bf4abdf7b3d3dcc84651ff22e
Luna Simons <luna@bddvlpr.com>
2024-02-11 21:35:06 +0100

    fix: use vt2 for greetd

--- hosts/common/greetd.nix
+++ hosts/common/greetd.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   services.greetd = {
     enable = true;
+    vt = 2;
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
