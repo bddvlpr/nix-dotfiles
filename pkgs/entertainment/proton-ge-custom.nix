59bd778b1c3cda4c26ad6823b889f8a93b665576
Luna Simons <luna@bddvlpr.com>
2024-02-12 03:11:11 +0100

    proton-ge-custom: 8-30 -> 8-32

--- pkgs/entertainment/proton-ge-custom.nix
+++ pkgs/entertainment/proton-ge-custom.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-30";
+  version = "GE-Proton8-32";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/${pname}/releases/download/${version}/${version}.tar.gz";
-    hash = "sha256-jt7snuw3rH2pnB70tRkuUCjvu4UF9tYUQOjTCHjYKXY=";
+    hash = "sha256-U+5CvBVWCFQRMtb7+KcaHx9Coj7cXg5V0EJd+e51PaA=";
   };
 
   buildCommand = ''
